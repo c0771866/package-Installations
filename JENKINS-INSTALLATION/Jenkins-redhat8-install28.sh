1b0c20d95158fcfd76914a501a542683472fcbb8
c0771866 <106362650+c0771866@users.noreply.github.com>
2022-09-05 02:02:26 -0400

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -1,10 +1,9 @@
 # CREATE HOSTNAME
 sudo hostnamectl set-hostname jenkins
-sudo yum install wget vim -y
+sudo yum install wget git vim -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum upgrade -y
 # Add required dependencies for the jenkins package
 sudo yum install java-11-openjdk -y
 sudo yum install jenkins -y
