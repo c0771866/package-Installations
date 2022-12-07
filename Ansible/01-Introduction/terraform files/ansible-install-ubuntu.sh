a6965e494e4b0256fd401b065bcdf32ce549b09f
c0771866 <106362650+c0771866@users.noreply.github.com>
2022-12-07 01:29:39 -0500

    Update ansible-install-ubuntu.sh

--- Ansible/01-Introduction/terraform files/ansible-install-ubuntu.sh
+++ Ansible/01-Introduction/terraform files/ansible-install-ubuntu.sh
@@ -6,3 +6,13 @@ echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible
 sudo su - ansible
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt install ansible -y
+sudo mkdir /etc/ansible
+sudo chown -R ansible:ansible /etc/ansible/
+# Install pip 
+pip3 install ansible --user
+pip3 install boto3 botocore --user
+pip3 install --upgrade requests --user
+# Create ansible HD
+vi /etc/ansible/ansible.cfg 
+# paste config file from official Ansible Github repo
+#uncheck host_key_checking = false
