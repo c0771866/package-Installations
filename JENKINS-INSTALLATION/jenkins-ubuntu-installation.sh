d8821cac62f58a58a4d3365a03ccb33b177c8e82
c0771866 <106362650+c0771866@users.noreply.github.com>
2023-01-25 00:20:23 -0500

    Update jenkins-ubuntu-installation.sh
    ubuntu install updated

--- JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
+++ JENKINS-INSTALLATION/jenkins-ubuntu-installation.sh
@@ -5,10 +5,11 @@ sudo apt update   # Update the repositories
 sudo apt install openjdk-11-jdk -y
 java -version
 # Install Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo dnf upgrade
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 # Add required dependencies for the jenkins package
 sudo dnf install chkconfig java-devel
 sudo dnf install jenkins
