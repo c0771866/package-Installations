2596bd1745c678e134aa49ad1379686245b4bd91
c0771866 <106362650+c0771866@users.noreply.github.com>
2023-01-25 11:56:49 -0500

    Update install-tomcat.sh

--- Tomcat-installation/install-tomcat.sh
+++ Tomcat-installation/install-tomcat.sh
@@ -7,7 +7,7 @@ sudo hostname tomcat
 # install Java JDK 1.8+ as a pre-requisit for tomcat to run.
 cd /opt 
 sudo yum install git wget -y
-sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install java-11-openjdk -y
 # Download tomcat software and extract it.
 sudo yum install wget unzip -y
 
